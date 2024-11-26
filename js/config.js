e0bdf806127c8ec13a6a03d2f6f07501f94bb6ca
Abu-Dabi <126262485+Abu-Dabi@users.noreply.github.com>
2024-11-26 11:26:10 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_iJy0owiCg', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3f791nlnidfbtqqfv3n8epgn19', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '75jgc5g82mhfap97oqq4oakelq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
